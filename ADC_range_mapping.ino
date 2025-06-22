aa182ddca2a11ce1e93e50f84ade35a7a9583ff9
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-06-22 10:21:57 +0800

    Update ADC_range_mapping.ino

--- ADC_range_mapping.ino
+++ ADC_range_mapping.ino
@@ -8,10 +8,10 @@
 #define led8 19
 #define buzz 23
 #define rly1 13
-#define rly2 12
-#define pb1  14
-#define pb2  27
-#define irs  26
+#define rly2 14
+#define pb1  27
+#define pb2  26
+#define irs  25
 #define var  34
 
 #define LED1(x) digitalWrite(led1, x? HIGH:LOW)
