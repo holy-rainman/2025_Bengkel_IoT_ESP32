42601a6df9a5c25cf704080a9ff24a25e9ab5800
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-07-02 22:00:46 +0800

    Update Blynk_PWM_slider.ino

--- Blynk_PWM_slider.ino
+++ Blynk_PWM_slider.ino
@@ -29,7 +29,7 @@ BLYNK_WRITE(V1)
 void setup()
 { for(uint8_t i=0;i<8;i++)
   { pinMode(leds[i],OUTPUT);
-    digitalWrite(leds[i],HIGH);
+    digitalWrite(leds[i],LOW);
   }
 
   Serial.begin(9600);
