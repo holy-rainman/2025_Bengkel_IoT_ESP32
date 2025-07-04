ab2f63085005f5f6e93f4658ff1882d83fc45601
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-07-04 23:18:56 +0800

    Create Edgent_Basic.ino

---
+++ Edgent_Basic.ino
@@ -0,0 +1,18 @@
+#define BLYNK_TEMPLATE_ID       "TMPL6GcQTz0fp"
+#define BLYNK_TEMPLATE_NAME     "Latihan IoT"
+#define BLYNK_FIRMWARE_VERSION  "0.1.0"
+#define BLYNK_PRINT Serial
+#define APP_DEBUG
+#define USE_ESP32_DEV_MODULE
+#include <WiFi.h>
+#include "BlynkEdgent.h"
+
+void setup()
+{ Serial.begin(9600);
+  BlynkEdgent.begin();
+}
+
+void loop() 
+{ BlynkEdgent.run();
+}
+
