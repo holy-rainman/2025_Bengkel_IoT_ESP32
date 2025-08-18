631c3e323cd4523ee3f1676b34fd7c1a361da950
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-08-18 13:43:12 +0800

    Update GoogleSheet_Blynk.ino

--- GoogleSheet_Blynk.ino
+++ GoogleSheet_Blynk.ino
@@ -231,9 +231,9 @@ void setup()
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
   timer.setInterval(200,kelip);
-  timer.setInterval(10,getADC);
+  timer.setInterval(1000,getADC);
   timer.setInterval(3000,getDHT11);
-  timer.setInterval(10,sendGoogleSheet);
+  timer.setInterval(1000,sendGoogleSheet);
 
   //============================== Google Sheet
   configTime(0, 0, ntpServer);
